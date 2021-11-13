bc713ac9ed8821125f3a7b1fd8e7e15e60d230db
ChineseWarlord <76110035+ChineseWarlord@users.noreply.github.com>
2021-11-13 23:29:08 +0100

    Add files via upload

--- WorkshopTestFolder/HovedProgram.c
+++ WorkshopTestFolder/HovedProgram.c
@@ -1,3 +1,5 @@
+//HovedProgram.c
+//https://github.com/ChineseWarlord/OS-Workshop.git
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -21,27 +23,26 @@ void* WriteFile(void* arg){ //Funktion som skriver ind i fil
     data = *(float*)arg;    //Sætter argumentet til at være lig med data.   - vi "caster" til float pointer og derefter dereference den
     fprintf(fp,"%s %0.1f C\n", format_time(), data); //Skriver data'en ind i filen
     fclose(fp); //Lukker filen
-    return 0;
+    pthread_exit(NULL); // Vi returner intet så vi kalder denne funktion
 }
 
 int main() {
     pthread_t T1, T2; //Deklarerer variabel T1/T2, af typen pthread_t
-    float Avg, *result;
+    float *result;
     int Timer = 5; //Antal sek der skal genereres data
 
     for (int i = 0; i < Timer; i++){ //Hvor lang tid programmet skal køre
         pthread_create(&T1, NULL, TempGen, NULL); //Opretter en tråd, der eksekverer funktionen 'TempGen'
-        pthread_join(T1, (void**)&result); //Tager en reference til en pointer, og sætter værdien til hvad end værdi der kommer fra TempGen funktionen
-        Avg = *result; //Lægger værdien fra result ind i float Avg
-        printf("Main result: %p\n", result); //Tjekker om addressen stemmer overens som i TempGenStub
+        pthread_join(T1, (void**)&result); //Returnerer pointeren altså returnerer en addresse.
+        printf("Main result: %0.01f\n", *result); //Tjekker om resultatet stemmer overens som i TempGenStub
+        printf("Main Address: %p\n", result); //Tjekker om addressen stemmer overens som i TempGenStub
 
-        pthread_create(&T2, NULL, WriteFile, &Avg); //Opretter en tråd og eksekverer funktionen 'WriteFile' som tager argumentet "Reference til variablen Avg (&Avg)"
-        pthread_join(T2, NULL); 
-        free(result); //Deallokere hukommelsen (frigører hukommelsen igen)
+        pthread_create(&T2, NULL, WriteFile, &*result); //Opretter en tråd og eksekverer funktionen 'WriteFile' som tager argumentet "Reference til variablen Avg (&Avg)"
+        pthread_join(T2, NULL); //Vent til thread T2 er færdig med at eksekvere funktionen, returner ingenting.
+        free(result);
         int sleep = usleep(1000000);
     }
-    //pthread_exit(0);
-    //exit(0);
+    
     return 0;
 }
 
