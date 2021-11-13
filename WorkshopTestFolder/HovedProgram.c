3aa3fbd407ad1f69170d662182d16bf97345a78d
ChineseWarlord <76110035+ChineseWarlord@users.noreply.github.com>
2021-11-13 13:22:45 +0100

    Add files via upload

--- WorkshopTestFolder/HovedProgram.c
+++ WorkshopTestFolder/HovedProgram.c
@@ -5,8 +5,7 @@
 #include "TempSensorStub.h"
 
 char curr_time[21];
-
-char* format_time(){
+char* format_time(){ //Funktion til at formatere tid: [dd/mm/yy hh:mm:ss]
     time_t rawtime;
     struct tm * timeinfo;time ( &rawtime );
     timeinfo = localtime (&rawtime);
@@ -14,30 +13,35 @@ char* format_time(){
     return curr_time;
 }
 
-void* WriteFile(void* arg){
-    FILE *fp;
+void* WriteFile(void* arg){ //Funktion som skriver ind i fil
+    FILE *fp;   //Deklarerer filpointer
     float data;
 
-    fp = fopen("Data.txt","a");
-    data= *(float*)arg;
-    fprintf(fp,"%s %0.1f C\n", format_time(), data);
-    fclose(fp);
+    fp = fopen("Data.txt","a"); //Opretter dokumentet "Data.txt" og åbner dokumentet i en "append-mode"
+    data = *(float*)arg;    //Sætter argumentet til at være lig med data.   - vi "caster" til float pointer og derefter dereference den
+    fprintf(fp,"%s %0.1f C\n", format_time(), data); //Skriver data'en ind i filen
+    fclose(fp); //Lukker filen
     return 0;
 }
 
 int main() {
-    pthread_t T1, T2;
+    pthread_t T1, T2; //Deklarerer variabel T1/T2, af typen pthread_t
     float Avg, *result;
     int Timer = 5; //Antal sek der skal genereres data
-    
-    for (int i = 0; i < Timer; i++){
-        pthread_create(&T1, NULL, TempGen, &Avg);
-        pthread_join(T1, (void **)&result);
-        Avg = *result;
-        pthread_create(&T2, NULL, WriteFile, &Avg);
-        free(result);
+
+    for (int i = 0; i < Timer; i++){ //Hvor lang tid programmet skal køre
+        pthread_create(&T1, NULL, TempGen, NULL); //Opretter en tråd, der eksekverer funktionen 'TempGen'
+        pthread_join(T1, (void**)&result); //Tager en reference til en pointer, og sætter værdien til hvad end værdi der kommer fra TempGen funktionen
+        Avg = *result; //Lægger værdien fra result ind i float Avg
+        printf("Main result: %p\n", result); //Tjekker om addressen stemmer overens som i TempGenStub
+
+        pthread_create(&T2, NULL, WriteFile, &Avg); //Opretter en tråd og eksekverer funktionen 'WriteFile' som tager argumentet "Reference til variablen Avg (&Avg)"
+        pthread_join(T2, NULL); 
+        free(result); //Deallokere hukommelsen (frigører hukommelsen igen)
         int sleep = usleep(1000000);
     }
-    pthread_exit(NULL);
-    exit(0);
+    //pthread_exit(0);
+    //exit(0);
+    return 0;
 }
+
--- WorkshopTestFolder/TempSensorStub.h
+++ WorkshopTestFolder/TempSensorStub.h
@@ -5,26 +5,27 @@
 #include <unistd.h>
 #include <time.h>
 
-void* TempGen(void* arg){
+void* TempGen(){ //Pointer til void som return type
     int Count = 10; //Antal målinger
     float Avg[Count], sum, n;
-    float avg = *(float*)arg;
+    float avg;
 
-    srand((unsigned int)time(NULL));
+    srand((unsigned int)time(NULL)); //Genererer pseudo-random numre
 
-    for(int i = 0; i < Count; i++){
-        n=((float)rand()/(float)RAND_MAX)*(50.0);
-        Avg[i] = n;
-        sum += Avg[i];
+    for(int i = 0; i < Count; i++){ //Temp generatoren
+        n=((float)rand()/(float)RAND_MAX)*(50.0); //Generer en random float fra 0.0 - 50.0
+        Avg[i] = n; //Indsæt i Avg array
+        sum += Avg[i]; //Tager summen
         //printf("Temp: %f\n", Avg[i]);
         int sleep = usleep(1*100000); //Vent 1 sekund
     }
-    avg = sum / Count;
+    avg = sum / Count; //Tag gennemsnittet og læg i avg variablen
     printf("Avg is: %0.1f\n", avg);
 
-    float *result = (float *)malloc(sizeof(result));
-    *result = avg;
+    float *result = (float *)malloc(sizeof(result)); //Laver en pointer 'result' og allokerer en blok af hukommelse. Den returnerer en pointer til den allokerede hukommelse "float" - svarer til 400 bytes
+    *result = avg; //Lægger værdien af avg over i pointeren result
+    printf("Thread result: %p\n", result); //Tjekker addressen
 
-    return result;
+    return (void*) result; //Returnerer void * (pointeren) result
 }
 #endif
